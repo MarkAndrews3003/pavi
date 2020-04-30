9335edecc389323f732e291de4d2d073aeb4576b
Mark Andrews <andrewsmarkdeveloper@gmail.com>
2020-04-30 11:34:40 +0400

    Added necessary assets, created home page

---
+++ src/assets/js/main.js
@@ -0,0 +1,32 @@
+new Glider(document.querySelector(".glider"), {
+    slidesToShow: 1.5,
+    slidesToScroll: 1,
+    draggable: true,
+    dots: ".dots",
+    responsive: [
+        {
+            breakpoint: 320,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                duration: 0.5,
+                arrows: {
+                    prev: ".glider-prev",
+                    next: ".glider-next"
+                }
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1.5,
+                slidesToScroll: 1,
+                duration: 0.5,
+                arrows: {
+                    prev: ".glider-prev",
+                    next: ".glider-next"
+                }
+            }
+        }
+    ]
+});
