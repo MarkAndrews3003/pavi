1e692a5a8826e91f9451d412c72fd9ef105440df
Mark Andrews <andrewsmarkdeveloper@gmail.com>
2020-04-30 15:55:59 +0400

    Minor fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,9 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './home/home.component';
-import {LoginComponent} from './auth/login/login.component';
 
 
 const routes: Routes = [
-  {
-    path: 'auth/login',
-    component: LoginComponent
-  },
   {
     path: '',
     component: HomeComponent
