217b85c2bd3ef81fa253983d9d598e2582b406e9
Mark Andrews <andrewsmarkdeveloper@gmail.com>
2020-05-05 14:10:20 +0400

    created sent-email and reset-password pages

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -9,10 +9,12 @@ import {Step1Component} from './company-sign-up/step1/step1.component';
 import {Step2Component} from './company-sign-up/step2/step2.component';
 import {Step3Component} from './company-sign-up/step3/step3.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { SentEmailComponent } from './sent-email/sent-email.component';
 
 
 @NgModule({
-  declarations: [UserSignUpComponent, CompanySignUpComponent, Step1Component, Step2Component, Step3Component],
+  declarations: [UserSignUpComponent, CompanySignUpComponent, Step1Component, Step2Component, Step3Component, ResetPasswordComponent, SentEmailComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
