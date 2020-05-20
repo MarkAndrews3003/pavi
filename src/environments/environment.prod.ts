60febb7140e2345706dad755dfa87ccf7f030a0b
Mark Andrews <andrewsmarkdeveloper@gmail.com>
2020-05-20 16:58:47 +0400

    Changed prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  apiUrl: 'https://pavi12.herokuapp.com/',
+  // apiUrl: 'https://pavi12.herokuapp.com/',
+  apiUrl: 'http://freelancedeveloper.site:3001/',
   // apiUrl: 'http://localhost:3000/',
   production: true
 };
