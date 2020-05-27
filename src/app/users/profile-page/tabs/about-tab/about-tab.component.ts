1e567829f3747e259e4360d5e04619bcb9cf4f7c
Mark Andrews <andrewsmarkdeveloper@gmail.com>
2020-05-27 12:43:15 +0400

    Birthday control fixes, installed moment

--- src/app/users/profile-page/tabs/about-tab/about-tab.component.ts
+++ src/app/users/profile-page/tabs/about-tab/about-tab.component.ts
@@ -1,16 +1,16 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {
-    API_URL,
     NUMBERS_ONLY_PATTERN,
-    OWL_CAROUSEL_OPTIONS,
     TEXT_ONLY_PATTERN
 } from '../../../../core/constants/general';
 import {GetAuthUserPipe} from '../../../../shared/pipes/get-auth-user.pipe';
 import {UsersService} from '../../../../core/services/users.service';
 import {ToastrService} from 'ngx-toastr';
 import {OwlOptions} from 'ngx-owl-carousel-o';
-import {patternValidator} from "../../../../core/helpers/pattern-validator";
+import {patternValidator} from '../../../../core/helpers/pattern-validator';
+import {COUNTRY_LIST} from '../../../../core/constants/countries';
+import * as  moment from 'moment';
 
 @Component({
     selector: 'app-about-tab',
@@ -26,6 +26,8 @@ export class AboutTabComponent implements OnInit {
     showChangePass = false;
     showChangeEmail = false;
     showEditProfileForm = false;
+    maxBirthdayDate = new Date(2009, 11, 31);
+    countries = COUNTRY_LIST;
 
 
     constructor(
@@ -36,7 +38,10 @@ export class AboutTabComponent implements OnInit {
     ) {
 
         this.profileForm = this.fb.group({
-            country: [{value: '', disabled: !this.showEditProfileForm}, [Validators.required, patternValidator(TEXT_ONLY_PATTERN)]],
+            country: [{
+                value: '',
+                disabled: !this.showEditProfileForm
+            }, [Validators.required, patternValidator(TEXT_ONLY_PATTERN)]],
             phone: [{
                 value: 'n/a',
                 disabled: !this.showEditProfileForm
@@ -44,6 +49,7 @@ export class AboutTabComponent implements OnInit {
             gender: [{value: '', disabled: true}, Validators.required],
             birthday: [{value: 'n/a', disabled: true}, Validators.required],
             email: [{value: '', disabled: true}, Validators.required],
+            age: [{value: '', disabled: true}]
 
         });
 
@@ -52,9 +58,18 @@ export class AboutTabComponent implements OnInit {
 
     ngOnInit(): void {
         this.authUser = this.getAuthUser.transform();
-        this.profileForm.patchValue(this.authUser)
-        console.log(this.authUser)
+        const age = this.countUsersAge();
+        this.profileForm.patchValue({...this.authUser, ...{age}});
 
+
+    }
+
+    countUsersAge() {
+        const now = moment(new Date());
+        const end = moment(this.authUser.birthday, 'YYYY-MM-DD'); // another date
+        const duration = moment.duration(now.diff(end));
+        const days = Math.floor(duration.asYears());
+        return days;
     }
 
     showChangePassForm() {
@@ -80,6 +95,7 @@ export class AboutTabComponent implements OnInit {
         this.showEditProfileForm = true;
         this.country.enable();
         this.phone.enable();
+        this.birthday.enable();
     }
 
 
@@ -90,13 +106,20 @@ export class AboutTabComponent implements OnInit {
         this.profileForm.patchValue(this.authUser);
     }
 
-    get phone() {
+    dateChanged(e) {
+        this.birthday.patchValue(moment(e.value).format('YYYY-MM-DD'));
+    }
+
+    get phone(): AbstractControl {
         return this.profileForm.get('phone');
     }
 
-    get country() {
+    get country(): AbstractControl {
         return this.profileForm.get('country');
     }
 
+    get birthday(): AbstractControl {
+        return this.profileForm.get('birthday');
+    }
 
 }
