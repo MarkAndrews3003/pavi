17237ea0d982cb04ce4ccc7eb46134052760c0f9
Mark Andrews <andrewsmarkdeveloper@gmail.com>
2020-05-27 11:09:29 +0400

    Fixes in change-password and change-mail password, the updated data now is stored in token in front end

--- src/app/users/profile-page/tabs/about-tab/about-tab.component.ts
+++ src/app/users/profile-page/tabs/about-tab/about-tab.component.ts
@@ -7,76 +7,79 @@ import {ToastrService} from 'ngx-toastr';
 import {OwlOptions} from 'ngx-owl-carousel-o';
 
 @Component({
-  selector: 'app-about-tab',
-  templateUrl: './about-tab.component.html',
-  styleUrls: ['./about-tab.component.css']
+    selector: 'app-about-tab',
+    templateUrl: './about-tab.component.html',
+    styleUrls: ['./about-tab.component.css']
 })
 export class AboutTabComponent implements OnInit {
 
-  authUser;
-  changePasswordForm: FormGroup;
-  changeEmailForm: FormGroup;
+    authUser;
 
-  profileForm: FormGroup;
+    profileForm: FormGroup;
 
-  showChangePass = false;
-  showChangeEmail = false;
-  showEditProfileForm = false;
-
-  owlOptions: OwlOptions = OWL_CAROUSEL_OPTIONS;
-  showAllJobs = false;
+    showChangePass = false;
+    showChangeEmail = false;
+    showEditProfileForm = false;
 
 
-  constructor(
-    private fb: FormBuilder,
-    private getAuthUser: GetAuthUserPipe,
-    private usersService: UsersService,
-    private toastr: ToastrService
-  ) {
 
-    this.profileForm = this.fb.group({
-      country: ['', Validators.required],
-      phone: ['', Validators.required],
-      // gender: ['', Validators.required],
-      // password: ['', Validators.required],
-      // email: ['', Validators.required],
-      gender: [{value: '', disabled: true}, Validators.required],
-      birthday: [{value: '', disabled: true}, Validators.required],
-      email: [{value: '', disabled: true}, Validators.required],
+    constructor(
+        private fb: FormBuilder,
+        private getAuthUser: GetAuthUserPipe,
+        private usersService: UsersService,
+        private toastr: ToastrService
+    ) {
 
-    });
+        this.profileForm = this.fb.group({
+            country: [{value: '', disabled: !this.showEditProfileForm}, Validators.required],
+            phone: [{value: 'n/a', disabled: !this.showEditProfileForm}, Validators.required],
+            gender: [{value: '', disabled: true}, Validators.required],
+            birthday: [{value: 'n/a', disabled: true}, Validators.required],
+            email: [{value: '', disabled: true}, Validators.required],
+
+        });
 
 
-  }
+    }
 
-  ngOnInit(): void {
-    this.authUser = this.getAuthUser.transform();
-    this.profileForm.patchValue(this.authUser)
-    console.log(this.authUser)
+    ngOnInit(): void {
+        this.authUser = this.getAuthUser.transform();
+        this.profileForm.patchValue(this.authUser)
+        console.log(this.authUser)
 
-  }
+    }
 
-  showChangePassForm() {
-    this.showChangePass = !this.showChangePass;
-  }
+    showChangePassForm() {
+        this.showChangePass = !this.showChangePass;
+    }
 
-  showChangeEmailForm() {
-    this.showChangeEmail = !this.showChangeEmail;
-  }
-
-
-  saveProfileDetails() {
-    console.log(this.profileForm.value)
-    this.usersService.updateProfileInfo(this.profileForm.value).subscribe(dt => {
-
-    });
-  }
+    showChangeEmailForm() {
+        this.showChangeEmail = !this.showChangeEmail;
+    }
 
 
-  backToMainForm() {
-    this.showChangePass = false;
-    this.showChangeEmail = false;
-  }
+    saveProfileDetails() {
+        this.usersService.updateProfileInfo({
+            ...this.profileForm.value, ...{user_id: this.authUser._id}
+        }).subscribe((dt: any) => {
+            localStorage.setItem('token', dt.token);
+            this.authUser = this.getAuthUser.transform();
+        });
+    }
+
+    editProfileForm() {
+        this.showEditProfileForm = true;
+        this.profileForm.get('country').enable();
+        this.profileForm.get('phone').enable();
+    }
+
+
+    backToMainForm() {
+        this.showChangePass = false;
+        this.showChangeEmail = false;
+        this.authUser = this.getAuthUser.transform();
+        this.profileForm.patchValue(this.authUser);
+    }
 
 
 }
--- src/app/users/profile-page/tabs/about-tab/change-password-form/change-password-form.component.ts
+++ src/app/users/profile-page/tabs/about-tab/change-password-form/change-password-form.component.ts
@@ -5,66 +5,70 @@ import {UsersService} from '../../../../../core/services/users.service';
 import {ToastrService} from 'ngx-toastr';
 
 @Component({
-  selector: 'app-change-password-form',
-  templateUrl: './change-password-form.component.html',
-  styleUrls: ['./change-password-form.component.css']
+    selector: 'app-change-password-form',
+    templateUrl: './change-password-form.component.html',
+    styleUrls: ['./change-password-form.component.css']
 })
 export class ChangePasswordFormComponent implements OnInit {
-  changePasswordForm: FormGroup;
-  passwordsMatch = true;
-  newOldPasswordsMatch = false;
-  isSubmitted = false;
+    changePasswordForm: FormGroup;
+    passwordsMatch = true;
+    newOldPasswordsMatch = false;
+    isSubmitted = false;
+    authUser;
 
-  @Output('back') back = new EventEmitter();
+    @Output('back') back = new EventEmitter();
 
-  constructor(
-    private getAuthUser: GetAuthUserPipe,
-    private fb: FormBuilder,
-    private usersService: UsersService,
-    private toastr: ToastrService
-  ) {
-    this.changePasswordForm = this.fb.group({
-      old_pass: ['', Validators.required],
-      confirm_password: ['', Validators.required],
-      new_pass: ['', Validators.required]
-    });
-  }
-
-  ngOnInit(): void {
-  }
-
-  comparePasswords() {
-    this.passwordsMatch = this.newPass.value === this.confirmPass.value;
-    this.compareNewOldPasswords();
-  }
-
-  compareNewOldPasswords() {
-    this.newOldPasswordsMatch = this.newPass.value && this.oldPass.value && this.newPass.value === this.oldPass.value;
-  }
-
-  changePassword() {
-    this.isSubmitted = true;
-    if (this.changePasswordForm.valid && !this.newOldPasswordsMatch && this.passwordsMatch) {
-      this.usersService.changePassword(this.changePasswordForm.value).subscribe(dt => {
-        this.toastr.success('Password has been changed successfully');
-      });
+    constructor(
+        private getAuthUser: GetAuthUserPipe,
+        private fb: FormBuilder,
+        private usersService: UsersService,
+        private toastr: ToastrService
+    ) {
+        this.changePasswordForm = this.fb.group({
+            old_pass: ['', Validators.required],
+            confirm_password: ['', Validators.required],
+            new_pass: ['', Validators.required]
+        });
     }
-  }
 
-  backToMainForm() {
-    this.back.emit();
-  }
+    ngOnInit(): void {
+        this.authUser = this.getAuthUser.transform();
+    }
 
-  get oldPass(): AbstractControl {
-    return this.changePasswordForm.get('old_pass');
-  }
+    comparePasswords() {
+        this.passwordsMatch = this.newPass.value === this.confirmPass.value;
+        this.compareNewOldPasswords();
+    }
 
-  get newPass(): AbstractControl {
-    return this.changePasswordForm.get('new_pass');
-  }
+    compareNewOldPasswords() {
+        this.newOldPasswordsMatch = this.newPass.value && this.oldPass.value && this.newPass.value === this.oldPass.value;
+    }
 
-  get confirmPass(): AbstractControl {
-    return this.changePasswordForm.get('confirm_password');
-  }
+    changePassword() {
+        this.isSubmitted = true;
+        if (this.changePasswordForm.valid && !this.newOldPasswordsMatch && this.passwordsMatch) {
+            this.usersService.changePassword({...this.changePasswordForm.value, ...{user_id: this.authUser._id}}).subscribe((dt: any) => {
+                localStorage.setItem('token', dt.token);
+                this.toastr.success('Password has been changed successfully');
+                this.backToMainForm();
+            });
+        }
+    }
+
+    backToMainForm() {
+        this.back.emit();
+    }
+
+    get oldPass(): AbstractControl {
+        return this.changePasswordForm.get('old_pass');
+    }
+
+    get newPass(): AbstractControl {
+        return this.changePasswordForm.get('new_pass');
+    }
+
+    get confirmPass(): AbstractControl {
+        return this.changePasswordForm.get('confirm_password');
+    }
 
 }
