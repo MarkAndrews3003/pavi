eb9f114bcbe1439d3f95264e67540cba38904591
Mark Andrews <andrewsmarkdeveloper@gmail.com>
2020-05-27 13:48:30 +0400

    created process to apply

--- src/app/jobs/jobs-routing.module.ts
+++ src/app/jobs/jobs-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ShowJobComponent } from './show-job/show-job.component';
-
+import { ApplyToJobComponent } from './apply-to-job/apply-to-job.component';
 const routes: Routes = [
   {
     path: 'show-job',
     component: ShowJobComponent
+  },
+  {
+    path: 'apply-to-job',
+    component: ApplyToJobComponent
   }
 ];
 
--- src/app/jobs/jobs.module.ts
+++ src/app/jobs/jobs.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { JobsRoutingModule } from './jobs-routing.module';
 import { ShowJobComponent } from './show-job/show-job.component';
 import {CoreModule} from '../core/core.module';
+import { ApplyToJobComponent } from './apply-to-job/apply-to-job.component';
 
 
 @NgModule({
-  declarations: [ShowJobComponent],
+  declarations: [ShowJobComponent, ApplyToJobComponent],
   imports: [
     CommonModule,
     JobsRoutingModule,
