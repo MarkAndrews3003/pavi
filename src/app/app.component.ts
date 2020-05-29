0362da5e5d81a6d92d8e3c25d7a27941ad3effe2
Mark Andrews <andrewsmarkdeveloper@gmail.com>
2020-05-29 09:40:03 +0400

    Added session expired message and checking for it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,43 +2,51 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {map} from 'rxjs/operators';
 import jwtDecode from 'jwt-decode';
+import {JwtHelperService} from "@auth0/angular-jwt";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'pavi';
+    title = 'pavi';
 
-  constructor(
-    public router: Router,
-    private route: ActivatedRoute
-  ) {
+    constructor(
+        public router: Router,
+        private route: ActivatedRoute,
+        private jwtHelper: JwtHelperService,
+        private toastr: ToastrService
+    ) {
 
-  }
+    }
 
-  ngOnInit(): void {
+    ngOnInit(): void {
 
-    this.router.events.pipe(map(() => {
-      let child = this.route.firstChild;
-      while (child) {
-        if (child.firstChild) {
-          child = child.firstChild;
-        } else if (child.snapshot.data) {
-          return child.snapshot;
-        } else {
-          return null;
-        }
-      }
-      return null;
-    })).subscribe(data => {
-      if (data) {
-        const token = data.queryParams.token;
-        if (token) {
-          localStorage.setItem('token', token);
-        }
-      }
-    });
-  }
+        this.router.events.pipe(map(() => {
+            let child = this.route.firstChild;
+            while (child) {
+                if (child.firstChild) {
+                    child = child.firstChild;
+                } else if (child.snapshot.data) {
+                    return child.snapshot;
+                } else {
+                    return null;
+                }
+            }
+            return null;
+        })).subscribe(data => {
+            if (data) {
+                const token = data.queryParams.token;
+                if (token) {
+                    localStorage.setItem('token', token);
+                }
+
+                if (this.jwtHelper.isTokenExpired()) {
+                    this.toastr.error('The session has been expired.', 'Please log in again.');
+                }
+            }
+        });
+    }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,26 +4,33 @@ import {AuthService} from '../core/services/auth.service';
 import {GetAuthUserPipe} from '../shared/pipes/get-auth-user.pipe';
 import {HOMEPAGE_OWL_CAROUSEL_OPTIONS} from '../core/constants/general';
 import {OwlOptions} from 'ngx-owl-carousel-o';
+import {JwtHelperService} from "@auth0/angular-jwt";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  authUser;
-  owlOptions: OwlOptions = HOMEPAGE_OWL_CAROUSEL_OPTIONS;
+    authUser;
+    owlOptions: OwlOptions = HOMEPAGE_OWL_CAROUSEL_OPTIONS;
 
-  constructor(
-    public router: Router,
-    public auth: AuthService,
-    private getAuthUser: GetAuthUserPipe
-  ) {
-  }
+    constructor(
+        public router: Router,
+        public auth: AuthService,
+        private getAuthUser: GetAuthUserPipe,
+        private jwtHelper: JwtHelperService
+    ) {
+    }
 
-  ngOnInit(): void {
-    this.authUser = this.getAuthUser.transform();
-  }
+    ngOnInit(): void {
+        console.log(this.jwtHelper.isTokenExpired())
+        this.authUser = this.getAuthUser.transform();
+    }
+
+    isTokenExpired() {
+        return !this.jwtHelper.isTokenExpired();
+    }
 
 }
