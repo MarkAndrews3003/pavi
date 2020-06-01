b44956b147f1e6b8dd3e98143bb86a0e9254b833
Mark Andrews <andrewsmarkdeveloper@gmail.com>
2020-06-01 11:05:22 +0400

    Added work experience form validation mostly

--- src/app/core/components/dialogs/save-work-experience-dialog/save-work-experience-dialog.component.ts
+++ src/app/core/components/dialogs/save-work-experience-dialog/save-work-experience-dialog.component.ts
@@ -2,6 +2,10 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ProfileService} from '../../../services/profile.service';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {patternValidator} from '../../../helpers/pattern-validator';
+import {NUMBERS_ONLY_PATTERN, TEXT_ONLY_PATTERN, YEAR_ONLY_PATTERN} from '../../../constants/general';
+
+import {isYearValid} from "../../../helpers/is-year-valid";
 
 @Component({
   selector: 'app-save-work-experience-dialog',
@@ -13,6 +17,7 @@ export class SaveWorkExperienceDialogComponent implements OnInit {
   workExperienceForm: FormArray;
   edit;
   editData;
+  isSubmitted = false;
 
   constructor(
     private fb: FormBuilder,
@@ -23,9 +28,9 @@ export class SaveWorkExperienceDialogComponent implements OnInit {
     this.workExperienceForm = this.fb.array([
       this.fb.group({
         company_name: ['', Validators.required],
-        speciality: ['', Validators.required],
-        start_year: ['', Validators.required],
-        end_year: ['', Validators.required]
+        speciality: ['', [Validators.required, patternValidator(TEXT_ONLY_PATTERN)]],
+        start_year: ['', [Validators.required, patternValidator(YEAR_ONLY_PATTERN), isYearValid()]],
+        end_year: ['', [Validators.required, patternValidator(YEAR_ONLY_PATTERN), isYearValid()]]
       })]);
 
     this.edit = !!data;
@@ -37,28 +42,41 @@ export class SaveWorkExperienceDialogComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    console.log(this.workExperienceForm.controls)
   }
 
   saveExperience() {
     const formValue = this.workExperienceForm.value;
+    this.isSubmitted = true;
+    console.log(formValue)
+    if (this.workExperienceForm.valid) {
+      if (!this.edit) {
 
-    if (!this.edit) {
+        this.profileService.addWorkExperience(formValue).subscribe(() => {
+          this.dialog.close();
+        });
 
-      this.profileService.addWorkExperience(formValue).subscribe(() => {
-        this.dialog.close();
-      });
+      } else {
+        formValue[0].index = this.editData.index;
+        this.profileService.updateWorkExperience(formValue[0]).subscribe(() => {
+          this.dialog.close();
+        });
 
-    } else {
-      formValue[0].index = this.editData.index;
-      this.profileService.updateWorkExperience(formValue[0]).subscribe(() => {
-        this.dialog.close();
-      });
-
+      }
     }
   }
 
   get experienceItems() {
     return this.workExperienceForm.controls;
   }
+
+  isYearValid(year) {
+    // let y = moment(year.value);
+    // if (y > moment()) {
+    //   console.log('invalid year')
+    // }
+    return 'OK';
+  }
+
 
 }
