b0ffb496da8a371f4fae92b58fa7a7684c5bbdb0
Mark Andrews <andrewsmarkdeveloper@gmail.com>
2020-06-02 15:02:28 +0400

    Job creation form stepper

---
+++ src/app/companies/dashboard/dashboard-jobs/job-creation-step1/job-creation-step1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-job-creation-step1',
+  templateUrl: './job-creation-step1.component.html',
+  styleUrls: ['./job-creation-step1.component.css']
+})
+export class JobCreationStep1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/companies/dashboard/dashboard-jobs/job-creation-step2/job-creation-step2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-job-creation-step2',
+  templateUrl: './job-creation-step2.component.html',
+  styleUrls: ['./job-creation-step2.component.css']
+})
+export class JobCreationStep2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
